4acb8fdc260396fd385cb9fab0a330dfc4305e14
vitorfreitas-cmyk <vitor.freitas@edu.senai.br>
2026-06-15 15:39:17 -0300

    1506

---
+++ Js/Meu_js.js
@@ -0,0 +1,9 @@
+
+        
+        //string
+        var texto= 'curso de java script';
+        //number
+        var numberInteiro = -7;
+        var numberFracionado = 10.45;
+        //boolean
+        var teste = true;
